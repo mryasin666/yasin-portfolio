37f2e9908438db06c7d7d75f3b3bb3a509cb861c
mryasin666 <babalalgoundi786@gmail.com>
2026-09-04 06:55:24 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  reveals.forEach((section) => {
+    const windowHeight = window.innerHeight;
+    const sectionTop = section.getBoundingClientRect().top;
+
+    if (sectionTop < windowHeight - 100) {
+      section.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
+revealOnScroll();
